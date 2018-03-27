03f1dca5b5c2e51abcb13e86c7889b06bba95377
Madeline Wong <madelinegwong@outlook.com>
2018-03-27 18:25:08 -0400

    matched exactly to the login page and created a redirect for farther subroutes of login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, {
-  Component
-} from 'react';
-import './App.css';
-import { Route, withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import "./App.css";
+import { Route, withRouter, Switch, Redirect } from "react-router-dom";
 
-import LoginPage from './pages/Login';
-import HomePage from './pages/Home';
-import SignupPage from './pages/Signup';
+import LoginPage from "./pages/Login";
+import HomePage from "./pages/Home";
+import SignupPage from "./pages/Signup";
 
-import Navigation from './components/Navigation';
+import Navigation from "./components/Navigation";
 
 class App extends Component {
   render() {
@@ -16,10 +14,13 @@ class App extends Component {
     return (
       <div>
         <Navigation />
-        <Route path='/' exact component={HomePage} />
-        <Route path='/login' component={LoginPage} />
-        <Route path='/signup' component={SignupPage} />
-        <Route path='/signup/:coupon' component={SignupPage} />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Redirect from="/login/*" to='/login'/>
+          <Route path="/login" exact component={LoginPage} />
+          <Route path="/signup" component={SignupPage} />
+          <Route path="/signup/:coupon" component={SignupPage} />
+        </Switch>
       </div>
     );
   }
